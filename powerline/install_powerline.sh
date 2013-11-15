10a89d719d2f86c2f3e31836058e2be110dbcd7d
Reuben D'Netto <rdnetto@gmail.com>
2013-11-15 18:24:20 +1100

    Added support for Debian/Ubuntu

--- powerline/install_powerline.sh
+++ powerline/install_powerline.sh
@@ -5,16 +5,26 @@ if [ $(whoami) == "root" ]; then
     exit
 fi
 
-#powerline itself
-sudo equo install zsh dev-python/pygit2 dev-python/psutil
-sudo pip install git+git://github.com/Lokaltog/powerline
-
 #fonts
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 sudo mv PowerlineSymbols.otf /usr/share/fonts/
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.avail/
 sudo fc-cache -vf
-sudo eselect fontconfig enable 10-powerline-symbols.conf
+
+if [ -x /usr/bin/equo ]; then
+    sudo equo install zsh dev-python/pygit2 dev-python/psutil
+    sudo eselect fontconfig enable 10-powerline-symbols.conf
+elif [ -x /usr/bin/apt-get ]; then
+    sudo apt-get install zsh pygit2 psutil python-pip python2.7-dev python-psutil
+    sudo pip install pygit2
+    cd /etc/fonts/conf.d
+    sudo ln -s /etc/fonts.avail/10-powerline
+else
+    echo "Could not find package manager"
+fi
+
+#powerline itself
+sudo pip install git+git://github.com/Lokaltog/powerline
 
 #not enabling vim plugin cause it's broken (ironically)
 
