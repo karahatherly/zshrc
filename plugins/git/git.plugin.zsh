024dd0e906df08319f12da9dbd8c9cf6f646d16e
Reuben D'Netto <rdnetto@gmail.com>
2014-11-16 16:15:42 +1100

    Added alias for git diff --no-index

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -7,6 +7,9 @@ alias gst='git status'
 compdef _git gst=git-status
 alias gd='git diff'
 compdef _git gd=git-diff
+alias gdn='git diff --no-index --color=always'
+compdef _git gdn=git-diff
+
 alias gdc='git diff --cached'
 compdef _git gdc=git-diff
 alias gl='git log'
@@ -19,7 +22,6 @@ alias gp='git push'
 compdef _git gp=git-push
 alias gpl='git pull'
 compdef _git gpl=git-pull
-alias gd='git diff'
 gdv() { git diff -w "$@" | view - }
 compdef _git gdv=git-diff
 alias gc='git commit -v'
