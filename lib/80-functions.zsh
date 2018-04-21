df5f449447dc7ecf2f8ef4e1c2f61410e3cd4224
Reuben D'Netto <rdnetto@gmail.com>
2018-04-21 18:20:30 +1000

    Added vfio-groups function

--- lib/80-functions.zsh
+++ lib/80-functions.zsh
@@ -103,3 +103,12 @@ function new-agent() {
     source /tmp/.ssh-agent
     ssh-add ~/.ssh/id_rsa
 }
+
+function vfio-groups() {
+    for d in /sys/kernel/iommu_groups/*/devices/*; do
+        n=${d#*/iommu_groups/*}; n=${n%%/*}
+        printf 'IOMMU Group %s ' "$n"
+        lspci -nns "${d##*/}"
+    done;
+}
+
