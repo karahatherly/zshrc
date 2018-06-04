611214db1d51d2f5453531fc716a3d0380d3c5a4
Reuben D'Netto <rdnetto@atlassian.com>
2018-06-04 11:29:40 +1000

    Added agJ

--- lib/90-aliases.zsh
+++ lib/90-aliases.zsh
@@ -35,6 +35,7 @@ alias rg="rg --smart-case                \
              --colors match:style:nobold "
 alias ag=rg
 alias agQ='rg -F'
+alias agJ='rg -t java'
 alias iotop='sudo iotop -o'
 alias mtr='/usr/sbin/mtr --curses -d'
 alias dmesg='dmesg -H'
