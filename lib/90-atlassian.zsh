482fe5c460787873bc245a153edb6fa21edc4413
Reuben D'Netto <rdnetto@atlassian.com>
2018-08-08 14:06:58 +1000

    Updated Jira aliases

--- lib/90-atlassian.zsh
+++ lib/90-atlassian.zsh
@@ -23,33 +23,22 @@ function reinit_peripherals() {
 # Aliases for grepping POMs / plugin.xml
 alias pom-grep="find . -iname pom.xml -print0 | xargs -0 -r ag"
 alias plugin-grep="find . -iname atlassian-plugin.xml -print0 | xargs -0 -r ag"
+alias cd-migrations='cd ./jira-components/jira-core/src/main/resources/db/platform/migrations/'
 
 # Aliases for connecting to Postgres in a docker container (started by jmake)
-alias pg-docker="./jmake postgres docker"
+alias pg-docker="./jmake postgres"
 alias pg-docker-restart="pg-docker stop; pg-docker start $@"
 alias pg-purge='docker rm -f $(cat docker.cid)'
 alias pgcli-docker="pgcli -h localhost -p 5433 jira jira"
+alias pgcli-d="pgcli -h localhost -p 5433 -U jira -d "
 alias psql-docker="psql -h localhost -p 5433 jira jira"
+alias psql-d="psql -h localhost -p 5433 -U jira -d "
 alias pgcli-sis='pgcli -h localhost -p 5435 postgres postgres'
 alias docker-cleanup='docker rm $(docker ps -a | awk "/Exited/{print $1}")'
 alias mci='mvn clean install -DskipTests'
+alias mcp='mvn clean package -DskipTests'
 
-# Aliases for starting JIRA
-alias jinstall="nice ./jmake install --frontend-skip"
-
-alias jdbg="nice ./jmake debug -J-Djira.webresource.local.caching=true --frontend-skip -J-Dplugin.webresource.batching.off=false"
-alias jdbg-indexprimary='jdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.indexprimary.enabled=true'
-alias jdbg-dbprimary='jdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.databaseprimary.enabled=true'
-alias jdbg-dbonly='jdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.databaseonly.enabled=true'
-alias jdbg-search-vertigo="jdbg -J-Dsearch.vertigo.mode=true"
-alias jdbg-vertigo="nice ./jmake vertigo debug -J-Djira.webresource.local.caching=true --frontend-skip -J-Dplugin.webresource.batching.off=false"
-
-alias jqdbg="./jmake debug quickstart -J-Djira.webresource.local.caching=true -J-Dplugin.webresource.batching.off=false"
-alias jqdbg-indexprimary='jqdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.indexprimary.enabled=true'
-alias jqdbg-dbprimary='jqdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.databaseprimary.enabled=true'
-alias jqdbg-dbonly='jqdbg -J-Djira.instrumentation.laas=true -J-Datlassian.darkfeature.jira.issue.search.api.databaseonly.enabled=true'
-alias jqdbg-search-vertigo="jqdbg -J-Dsearch.vertigo.mode=true"
-alias jqdbg-vertigo="nice ./jmake vertigo debug quickstart -J-Djira.webresource.local.caching=true --frontend-skip -J-Dplugin.webresource.batching.off=false"
+alias jmake_alpha='export JMAKE_VERSION=$(xpath -q -e "/project/version/text()" ~/jmake/pom.xml)'
 
 # go/build-status-in-a-shell
 alias builds='$HOME/sources/build-status-in-a-shell/cli/build-status.py --list'
