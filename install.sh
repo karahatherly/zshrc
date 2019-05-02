828fd4e2e4b4236933913596badd058ede518a4d
R D'Netto <rdnetto@gmail.com>
2019-05-02 17:56:00 +1000

    Added Dracula colourscheme

--- install.sh
+++ install.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
+set -e
 
-ZSH=$(dirname $(realpath $0))
-pushd $ZSH >/dev/null
+ZSH="$(dirname "$(realpath "$0")")"
+pushd "$ZSH" >/dev/null
 
 # XDG config directories
 mkdir -p ~/.config
 cd ~/.config
-[ -d ~/.config/powerline ] || ln -sf $ZSH/conf/powerline
-[ -d ~/.config/git ] || ln -sf $ZSH/conf/git
-[ -d ~/.config/nvim ] || ln -sf ~/.vim
+[ -d ~/.config/powerline ] || ln -sf "$ZSH/conf/powerline" .
+[ -d ~/.config/git ] || ln -sf "$ZSH/conf/git" .
+[ -d ~/.config/nvim ] || ln -sf ~/.vim .
 
 # RC files
-cd $HOME
-ln -sf $ZSH/conf/tmux.conf .tmux.conf
-ln -sf $ZSH/conf/tigrc .tigrc
-ln -sf $ZSH/zshenv .zshenv
-ln -sf $ZSH/zshenv .zprofile
-ln -sf $ZSH/zshenv .profile
-ln -sf $ZSH/zshrc .zshrc
+cd "$HOME"
+ln -sf "$ZSH/conf/tmux.conf" .tmux.conf
+ln -sf "$ZSH/conf/tigrc" .tigrc
+ln -sf "$ZSH/zshenv" .zshenv
+ln -sf "$ZSH/zshenv" .zprofile
+ln -sf "$ZSH/zshenv" .profile
+ln -sf "$ZSH/zshrc" .zshrc
 
 popd >/dev/null
+
+# Colour scheme for Konsole
+if ! [ -d ~/.local/share/konsole/Dracula.colorscheme ] ; then
+    mkdir -p ~/.local/share/konsole
+    pushd ~/.local/share/konsole
+    curl "https://raw.githubusercontent.com/dracula/konsole/master/Dracula.colorscheme" > Dracula.colorscheme
+
+    [ -f Shell.profile ] && sed -i 's/ColorScheme.*/ColorScheme=Dracula/' Shell.profile
+
+    popd >/dev/null
+fi
 
 sudo layman -o 'https://ebuilds.janchren.eu/repos/rindeal/repositories.xml' -f -a rindeal
 sudo emerge -avt diff-so-fancy
