89b358da45577897b9981ac1d7b40d116a9eef45
Renee D'Netto <rdnetto@gmail.com>
2020-08-24 01:33:13 +1000

    Replaced external powerline script with inline one

---
+++ lib/60-powerline.zsh
@@ -0,0 +1,60 @@
+# Based on https://github.com/powerline/powerline/blob/develop/powerline/bindings/zsh/powerline.zsh
+# Inlining it here eliminates the runtime dependency on the powerline package
+
+_powerline_set_jobnum() {
+	# If you are wondering why I am not using the same code as I use for bash
+	# ($(jobs|wc -l)): consider the following test:
+	#     echo abc | less
+	#     <C-z>
+	# . This way jobs will print
+	#     [1]  + done       echo abc |
+	#            suspended  less -M
+	# ([ is in first column). You see: any line counting thingie will return
+	# wrong number of jobs. You need to filter the lines first. Or not use
+	# jobs built-in at all.
+	integer -g _POWERLINE_JOBNUM=${(%):-%j}
+}
+
+_powerline_columns_fallback() {
+	if which stty &>/dev/null ; then
+		local cols="$(stty size 2>/dev/null)"
+		if ! test -z "$cols" ; then
+			echo "${cols#* }"
+			return 0
+		fi
+	fi
+	echo 0
+	return 0
+}
+
+# Configure shell
+setopt promptpercent
+setopt promptsubst
+
+# Setup prompt
+precmd_functions+=( _powerline_set_jobnum )
+typeset -g VIRTUAL_ENV_DISABLE_PROMPT=1
+local POWERLINE_COMMAND=powerline-hs
+
+local add_args='-r .zsh'
+add_args+=' --last-exit-code=$?'
+add_args+=' --last-pipe-status="$pipestatus"'
+add_args+=' --renderer-arg="client_id=$$"'
+add_args+=' --renderer-arg="shortened_path=${(%):-%~}"'
+add_args+=' --jobnum=$_POWERLINE_JOBNUM'
+add_args+=' --renderer-arg="mode=$_POWERLINE_MODE"'
+add_args+=' --renderer-arg="default_mode=$_POWERLINE_DEFAULT_MODE"'
+local new_args_2=' --renderer-arg="parser_state=${(%%):-%_}"'
+new_args_2+=' --renderer-arg="local_theme=continuation"'
+local add_args_3=$add_args' --renderer-arg="local_theme=select"'
+local add_args_2=$add_args$new_args_2
+add_args+=' --width=$(( ${COLUMNS:-$(_powerline_columns_fallback)} - ${ZLE_RPROMPT_INDENT:-1} ))'
+local add_args_r2=$add_args$new_args_2
+
+# Set the prompts
+typeset -g PS1='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell aboveleft '$add_args')'
+typeset -g RPS1='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell right '$add_args')'
+typeset -g PS2='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell left '$add_args_2')'
+typeset -g RPS2='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell right '$add_args_r2')'
+typeset -g PS3='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell left '$add_args_3')'
+
--- zshrc
+++ zshrc
@@ -23,39 +23,6 @@ else
     echo "Antibody not installed" >&2
 fi
 
-# Load powerline
-# TODO: move this into a plugin
-# --------------------------------------------------------------------------------
-if cmd_exists powerline-hs; then
-    TTY="$(tty | awk -F/ '{print $3;}')"
-
-    # Path in: raiagent overlay, Debian, Gentoo
-    PYTHON_VER="$(python3 --version | grep -o '3..')"
-    for POWERLINE_PATH in "/usr/share/zsh/site-contrib/powerline.zsh" "/usr/share/powerline/bindings/zsh/powerline.zsh" "/usr/lib/python${PYTHON_VER}/site-packages/powerline/bindings/zsh/powerline.zsh" ; do
-        [[ -r "$POWERLINE_PATH" ]] && break
-    done
-
-    [ -f /etc/vconsole.conf ] && source /etc/vconsole.conf
-
-    if [[ ! -r $POWERLINE_PATH ]] ; then
-        echo "Powerline is not installed."
-        POWERLINE_PATH="/dev/null"
-    elif [ "$TTY" != "pts" -a "$FONT" != "eurlatgr" ]; then
-        # We need a console font that supports unicode
-        echo "Running on $(tty) with unsupported font. Please edit /etc/vconsole.conf to use eurlatgr."
-        setfont eurlatgr
-    fi
-
-    POWERLINE_COMMAND=powerline-hs
-    POWERLINE_CONFIG_COMMAND=/bin/true
-    source $POWERLINE_PATH
-
-    unset FONT KEYMAP POWERLINE_PATH TTY
-else
-    echo "Powerline-hs not installed" >&2
-fi
-# --------------------------------------------------------------------------------
-
 # Load work key
 if [ -f /media/Work/ssh-key/load.sh ]; then
     source /media/Work/ssh-key/load.sh
